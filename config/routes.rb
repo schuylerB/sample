b963e02d9085283b5e757e411fd053fde2ab384c
Schuyler Burson <Schuyler.Burson@gmail.com>
2013-06-09 13:20:45 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  get "users/new"  
+
+  root :to => 'static_pages#home' 
+
+  match '/signup' => 'users#new'
+
+  match '/help' => 'static_pages#help'
+  match '/about' => 'static_pages#about'
+  match '/contact' => 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
